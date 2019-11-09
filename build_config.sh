48f36c0e4e5249e631534f69a7d32f25eccc6884
linsjb <linussjobro@gmail.com>
2019-11-09 15:49:01 +0100

    Changes

--- build_config.sh
+++ build_config.sh
@@ -1,15 +1,14 @@
 
 # Oh-my-zsh installation
- wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-sh install.sh --skip-chsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 
 # Clone ZSH theme and plugins
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Build symlinks
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 ln -s ~/.dotfile_configs/vim/vimrc ~/.vimrc
 rm ~/.zshrc
 ln -s ~/.dotfile_configs/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfile_configs/hyper/hyper.js ~/.hyper.js
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
