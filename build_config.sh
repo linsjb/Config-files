3471e6ff9191a6c5cdae668d854925231b2d3ef5
linsjb <linussjobro@gmail.com>
2019-11-14 15:13:18 +0100

    New docker aliases + oh-my-zsh highlight plugin

--- build_config.sh
+++ build_config.sh
@@ -4,8 +4,9 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 
 # Clone ZSH theme and plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/denysdovhan/spaceship-prompt.git $HOME/.oh-my-zsh/custom/themes/spaceship-prompt
+
 
 # Build symlinks
 ln -s ~/.dotfile_configs/vim/vimrc ~/.vimrc
